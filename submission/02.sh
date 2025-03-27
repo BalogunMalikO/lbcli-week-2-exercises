498d774a90fe3b6896c26715bf48c42cc1e0dfc4
BalogunMalikO <balogunmalik4@gmail.comgit>
2025-03-27 15:28:12 +0100

    week-one

--- submission/02.sh
+++ submission/02.sh
@@ -1,5 +1,6 @@
 # Create a new Bitcoin address, for receiving change.
 # Using legacy address type as specified
+bitcoin-cli -regtest createwallet "btruswallet"
 CHANGE_ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btruswallet getnewaddress "" "legacy")
 
 echo "New change address created: $CHANGE_ADDRESS"
