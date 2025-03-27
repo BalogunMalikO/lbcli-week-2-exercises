d0e68c3ea9fe3e374189f12a146c19660a8ff6dd
BalogunMalikO <balogunmalik4@gmail.comgit>
2025-03-27 15:50:12 +0100

    week-one

--- submission/02.sh
+++ submission/02.sh
@@ -1,10 +1,10 @@
 
 
 # Create a new Bitcoin address, for receiving change.
-# Using native SegWit (bech32) address type for regtest
+
 
 CHANGE_ADDRESS=$(bitcoin-cli -regtest getnewaddress)
 
-echo "New change address created: $CHANGE_ADDRESS"
+
 
 
