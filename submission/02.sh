1cbd3e773c1e894a79f04a4e4bc47d4b5aa81835
BalogunMalikO <balogunmalik4@gmail.comgit>
2025-03-27 15:37:39 +0100

    week-one

--- submission/02.sh
+++ submission/02.sh
@@ -1,16 +1,10 @@
-#!/bin/bash
+
 
 # Create a new Bitcoin address, for receiving change.
 # Using native SegWit (bech32) address type for regtest
-bitcoin-cli -regtest createwallet "btruswallet"
-CHANGE_ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btruswallet getnewaddress "" "bech32")
+bitcoin-cli -regtest loadwallet "btrust"
+CHANGE_ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btrust getnewaddress "" "bech32")
 
 echo "New change address created: $CHANGE_ADDRESS"
 
-# Verify the address format
-if [[ "$CHANGE_ADDRESS" =~ ^bcrt1[ac-hj-np-z02-9]{8,87}$ ]]; then
-    echo "✅ Success: Change address generation passed!"
-else
-    echo "❌ Error: Change address generation failed!"
-    exit 1
-fi
+
