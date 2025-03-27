ac3c7bbfb8cf63859849b9742995b17edb7bc04f
BalogunMalikO <balogunmalik4@gmail.comgit>
2025-03-27 15:57:12 +0100

    week-one

--- submission/03.sh
+++ submission/03.sh
@@ -1,7 +1,7 @@
 # Created a SegWit address.
 SEGWIT_ADDR=$(bitcoin-cli -regtest getnewaddress "" "bech32")
 # Add funds to the address.
-mine_blocks 101 $SEGWIT_ADDR
+bitcoin-cli mine_blocks 101 $SEGWIT_ADDR
 
 # Return only the Address
 
