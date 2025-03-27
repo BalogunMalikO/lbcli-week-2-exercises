0acdd0db30a0a1d2c719681cd48f52838e9589f7
BalogunMalikO <balogunmalik4@gmail.comgit>
2025-03-27 14:40:30 +0100

    week-one

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,8 @@
 # Created a SegWit address.
+SEGWIT_ADDR=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32")
 # Add funds to the address.
+mine_blocks 101 $SEGWIT_ADDR
+
 # Return only the Address
+
+echo "THIS IS MY SEGWIT ADDRESS: $SEGWIT_ADDR"
--- submission/04.sh
+++ submission/04.sh
@@ -1,1 +1,6 @@
 # List the current UTXOs in your wallet.
+
+UTXO =($bitcoin-cli -regtest -rpcwallet=btruswallet listunspent)
+
+echo "THIS IS UTXO: $UTXO"
+
