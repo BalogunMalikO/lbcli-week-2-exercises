b3d30eaf30b4acc54143bc9ab550d836838dea65
BalogunMalikO <balogunmalik4@gmail.comgit>
2025-03-27 14:59:15 +0100

    week-one

--- submission/02.sh
+++ submission/02.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
 # Create a new Bitcoin address, for receiving change.
-bitcoin-cli -regtest -rpcwallet=btruswallet getnewaddress "" "bech32"
+# Using bech32 address type for better efficiency and lower fees
+CHANGE_ADDRESS=$(bitcoin-cli -regtest -rpcwallet=btruswallet getnewaddress "" "bech32")
+
+echo "New change address created: $CHANGE_ADDRESS"
